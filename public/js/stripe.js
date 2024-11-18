a7aacd6cdd657dde47a6d2fab15447af824fcf35
Ivan Kovalyov <ivan.kovalyov@doxy.me>
2024-11-18 12:48:11 +0200

    update bundle

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,19 +1,20 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
-const stripe = Stripe('pk_test_51QKaCm01wCp86y9P0kn69ZLojjvQXzdcsm6AGH1WKKTfACIff01vaQsLquOA3rSrKwtdrXkiw7gaxxlB6URfTXYq007QEW3bIV');
+const stripe = Stripe(
+  'pk_test_51QKaCm01wCp86y9P0kn69ZLojjvQXzdcsm6AGH1WKKTfACIff01vaQsLquOA3rSrKwtdrXkiw7gaxxlB6URfTXYq007QEW3bIV',
+);
 
-export const bookTour = async tourId => {
+export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
     const session = await axios(
-      `http://127.0.0.1:8000/api/v1/bookings/checkout-session/${tourId}`
+      `/api/v1/bookings/checkout-session/${tourId}`,
     );
-    console.log(session);
 
     // 2) Create checkout form + chanre credit card
     await stripe.redirectToCheckout({
-      sessionId: session.data.session.id
+      sessionId: session.data.session.id,
     });
   } catch (err) {
     console.log(err);
